a612349977c51a883235b3d90396de2052f47bfd
Hemant Pandey <hpandey110301@gmail.com>
2021-09-27 13:19:48 +0530

    Exp-3 Binomial Heap extract_min, meld heap implemented

--- exp-3/src/bin/binomial-heap.rs
+++ exp-3/src/bin/binomial-heap.rs
@@ -5,7 +5,8 @@ use std::cmp::Ordering;
 pub enum Error {
     DegreeMismatch,
     EmptyTree,
-    AddToEmpty
+    AddToEmpty,
+    EmptyHeap
 }
 
 
@@ -42,6 +43,12 @@ impl<T: Ord> BinomialTree<T> {
         }
     }
 
+    pub fn get_degree(&self) -> Option<usize> {
+        match self {
+            BinomialTree::Node(node) => Some(node.degree),
+            BinomialTree::Empty => None
+        }
+    }
     // No need to implement insert. Inserting into a binomial tree will not be supported...
     // Meld two binomial trees of degree k - 1 into one tree of degree k
     pub fn meld(self, b: BinomialTree<T>) -> Result<BinomialTree<T>, Error>{
@@ -67,63 +74,115 @@ impl<T: Ord> BinomialTree<T> {
             (_, _) => Err(Error::EmptyTree)
         }
     }
+
+    // Fracture a binomial tree and return the root node value alongside
+    pub fn fracture(self) -> Result<(T, Vec<Box<BinomialTree<T>>>), Error> {
+        match self {
+            BinomialTree::Node(node) => Ok((node.value, node.children)),
+            BinomialTree::Empty => Err(Error::EmptyTree)
+        }
+    }
 }
 
 pub struct BinomialHeap<T: Ord> {
     node_list: Vec<BinomialTree<T>>,
-    n: usize
+    non_empty: usize
 } 
 
 impl<T: Ord> BinomialHeap<T> {
     pub fn new() -> Self {
         BinomialHeap {
             node_list: Vec::new(),
-            n: 0
+            non_empty: 0
         }
     }
+
+    // Insert a tree of degree k into the heap
     pub fn insert_tree(&mut self, k: usize, tree: BinomialTree<T>) -> Result<(), Error>{
-        if k == self.n {
+        if k == self.node_list.len() {
             self.node_list.push(tree);
-            self.n += 1;
+            self.non_empty += 1;
             Ok(())
         } else {
             match &self.node_list[k] {
                 BinomialTree::Node(_) => {
                     let mut temp_tree = std::mem::replace(&mut self.node_list[k], BinomialTree::Empty);
+                    self.non_empty -= 1;
                     temp_tree = temp_tree.meld(tree)?;
                     self.insert_tree(k + 1, temp_tree)?;
                 },
                 BinomialTree::Empty => {
                     self.node_list[k] = tree;
+                    self.non_empty += 1;
                 }
             }
-            Ok(())
+            return Ok(());
         }
     }
     
+    // Insert an element into the heap
     pub fn insert(&mut self, value: T) -> Result<(), Error>{
         return self.insert_tree(0, BinomialTree::new(value));
     }
 
-    pub fn find_min(&self, k: usize) -> Option<&T> {
-        if k >= self.n {
-            None
-        } else {
-            
-            match (self.node_list[k].get_min(), self.find_min(k + 1)) {
-                (Some(c_min), Some(s_min)) => {
-                    match c_min.cmp(s_min) {
-                        Ordering::Greater => Some(s_min),
-                        _ => Some(c_min)
+    // Find the position of binomial tree with min elem
+    pub fn find_min_idx(&self) -> Result<usize, Error> {
+        if self.non_empty == 0 {
+            return Err(Error::EmptyHeap);
+        }
+        let mut min_idx = 0;
+        for i in 0..self.node_list.len() {
+            match (self.node_list[i].get_min(), self.node_list[min_idx].get_min()) {
+                (_, None) => {
+                    min_idx = i;
+                },
+                (Some(v), Some(u)) => {
+                    if v < u {
+                        min_idx = i;
                     }
                 },
-                (None, Some(s_min)) => Some(s_min),
-                (Some(c_min), None) => Some(c_min),
-                _ => None
+                (None, _) => {}
             }
         }
+        return Ok(min_idx);
     }
-    //pub fn extract_min(&mut self, k: usize)
+
+    // Find an immutable reference to the minimum value in the heap
+    pub fn find_min(&self) -> Option<&T> {
+        match self.find_min_idx() {
+            Ok(v) => self.node_list[v].get_min(),
+            _ => None
+        }
+    }
+
+    // Removes smallest element from heap and returns it
+    pub fn extract_min(&mut self) -> Result<T, Error> {
+
+        let min_idx = self.find_min_idx()?;
+        let min_idx_tree = std::mem::replace(&mut self.node_list[min_idx], BinomialTree::Empty);
+        self.non_empty -= 1;
+        let (min_value, trees) = min_idx_tree.fracture()?;
+        for tree_boxed in trees.into_iter() {
+            let tree = *tree_boxed;
+            self.insert_tree(tree.get_degree().unwrap(), tree)?;
+        }
+        return Ok(min_value);
+    }
+
+    pub fn meld(mut self, b: BinomialHeap<T>) -> Result<Self, Error> {
+        if self.non_empty == 0 || b.non_empty == 0 {
+            return Err(Error::EmptyHeap);
+        }
+        for tree in b.node_list.into_iter() {
+            match tree {
+                BinomialTree::Node(node) => {
+                    self.insert_tree(node.degree, BinomialTree::Node(node))?;
+                },
+                BinomialTree::Empty => {}
+            }
+        }
+        return Ok(self);
+    }
 }
 
 fn main() {
@@ -138,12 +197,20 @@ fn main() {
                             .map(|s| s.trim().parse().unwrap())
                             .collect();
     let mut bheap: BinomialHeap<i32> = BinomialHeap::new();
+    let mut cheap: BinomialHeap<i32> = BinomialHeap::new();
     for num in nums.iter() {
-        bheap.insert(*num).unwrap();
+        if num%2 == 0{
+            cheap.insert(*num).unwrap();
+        }
+        else {
+            bheap.insert(*num).unwrap();
+        }
     }
-    match bheap.find_min(0) {
-        Some(x) => println!("{}", x),
-        None => ()
+    println!("non empty trees a : {} b : {}", cheap.non_empty, bheap.non_empty);
+    bheap = bheap.meld(cheap).unwrap();
+    while bheap.non_empty > 0 {
+        let temp = bheap.extract_min().unwrap();
+        print!("{} ", temp);
     }
     
 }
